a93c417cbdba951dcc284ec9aafda4753d069ed0
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-10-16 09:36:13 -0500

    spell ENV correctly lol

--- dbconfig.js
+++ dbconfig.js
@@ -1,6 +1,6 @@
 const knex = require("knex");
 
-const environment = process.env.NODE_env || 'development'
+const environment = process.env.NODE_ENV || 'development'
 const config = require("./knexfile")[environment];
 
 module.exports = knex(config); 
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const helmet = require('helmet')
-const cors = require('cors')
+// const cors = require('cors')
 const userRouter = require('./routers/userRouter')
-const session = require('express-session')
+// const session = require('express-session')
 
 server = express()
 
-
+console.log('enviornment:', process.env.NODE_ENV)
 server.use(helmet())
-server.use(cors())
+// server.use(cors())
 server.use(express.json())
+
+
+server.use('/api', userRouter)
+
+module.exports = server;
+
+
+
+
+
+
 
 // const sessionConfig = {
 //     name: "chocochip",
@@ -29,6 +40,3 @@ server.use(express.json())
 // server.use(session(sessionConfig))
 
 
-server.use('/api', userRouter)
-
-module.exports = server;
