ef0ba00cd368608b67e68fc6c4acb7d49c107df5
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-10-16 17:57:10 -0500

    change .env and knex string

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-
+require('dotenv').config()
 module.exports = {
 
   development: {
@@ -27,13 +27,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-      filename: process.env.DATABASE_URL,
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./data/migrations"
     },
