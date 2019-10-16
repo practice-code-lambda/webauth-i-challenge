aa45afed29ace52a559085161aafbcd416c98436
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-10-16 12:26:42 -0500

    add production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -27,4 +27,29 @@ module.exports = {
       directory: "./data/seeds"
     }
   },
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
 }
